692bf7d7fd501f5a855f2030d0ed649c5a33055e
echao12 <echao0077@gmail.com>
2021-11-16 22:17:57 -0800

    updated repository to provide basic get functionality

--- Repositories/InMemItemsRepository.cs
+++ Repositories/InMemItemsRepository.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using Catalog.Entities;
+using System.Linq;
 
 namespace Catalog.Repositories {
     public class InMemoryRepository
@@ -11,5 +12,16 @@ namespace Catalog.Repositories {
             new Item {Id = Guid.NewGuid(), Name = "Iron Sword", Price = 20, CreatedDate = DateTimeOffset.UtcNow },
             new Item {Id = Guid.NewGuid(), Name = "Bronze Shield", Price = 18, CreatedDate = DateTimeOffset.UtcNow },
         };
+
+        //get all items
+        public IEnumerable<Item> GetItems() {
+            return items;
+        }
+
+        public Item GetItem(Guid id) {
+            //Where() returns a collection. use SingleOrDefault() to get the single element.
+            //returns null if not found.
+            return items.Where(item => item.Id == id).SingleOrDefault();
+        }
     }
 }
