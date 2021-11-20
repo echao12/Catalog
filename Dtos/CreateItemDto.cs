c93c91a8eb19d4c11131b1b112477763f95350bd
echao12 <echao0077@gmail.com>
2021-11-20 00:04:07 -0800

    added Data Annotations
    client must supply a name and price within [1,1000]

--- Dtos/CreateItemDto.cs
+++ Dtos/CreateItemDto.cs
@@ -1,9 +1,14 @@
+using System.ComponentModel.DataAnnotations; // enables [Required] & [Range()] annotation
+
 namespace Catalog.Dtos {
     public record CreateItemDto {
         //note: Guid & CretedTime should be generated on the server side
 
         //client should pass name and price
+        [Required] //must be provided
         public string Name {get; init;} 
+        [Required]
+        [Range(1,1000)] // restrict valid values to be b/t [1 - 1000]
         public decimal Price {get; init;}
     }
 }
