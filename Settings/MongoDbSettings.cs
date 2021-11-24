f6ef13ff9c41a2d1354c90253aa6d13274f90282
echao12 <echao0077@gmail.com>
2021-11-23 16:59:33 -0800

    Added authentication to DB
    used .NET Secrets manager to add a username / password to access database.
    modified mongodbsettings file to include "user"
    added a "password : " entry to secrets manager which matches the MongoDbSettings entry in appsettings.json

--- Settings/MongoDbSettings.cs
+++ Settings/MongoDbSettings.cs
@@ -2,9 +2,12 @@ namespace Catalog.Settings {
     public class MongoDbSettings {
         public string Host {get; set;}
         public int Port {get; set;}
+        public string User {get; set;} // will be populated by .NET during runtime
+        public string Password {get; set;} // will be populated by .NET during runtime
         public string ConnectionString { 
             get {
-                return $"mongodb://{Host}:{Port}";
+                //format mongo is expecting
+                return $"mongodb://{User}:{Password}@{Host}:{Port}";
             }
         }
     }
